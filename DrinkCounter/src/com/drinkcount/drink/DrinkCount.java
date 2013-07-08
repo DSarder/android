8f6e744fa9f83ca62c89efc9473c24778ee21097
Daniel <Daniel@Daniel-PC>
2013-07-08 19:50:31 +1000

    Initial DrinkRack

---
+++ DrinkCounter/src/com/drinkcount/drink/DrinkCount.java
@@ -0,0 +1,18 @@
+package com.drinkcount.drink;
+
+import java.util.ArrayList;
+
+public class DrinkCount {
+	private ArrayList<Drink> drinkRack;
+
+	public ArrayList<Drink> getDrinkRack() {
+		return drinkRack;
+	}
+
+	public void setDrinkRack(ArrayList<Drink> drinkRack) {
+		this.drinkRack = drinkRack;
+	}
+	
+	
+
+}
